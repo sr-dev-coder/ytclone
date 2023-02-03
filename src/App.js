253c230bb1667b838c11cdf95ed377ea8af0a115
Sandeep Rohilla <81100360+sr-dev-coder@users.noreply.github.com>
2023-02-03 22:24:27 +0530

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './componen
 
 const App = () => {
   return <BrowserRouter>
-    <Box sx={{ backgroundColor: '#000' }}>
+    <Box sx={{ backgroundColor: '#000' }} >
         <Navbar />
         <Routes>
             <Route path="/" exact element={<Feed />} />
