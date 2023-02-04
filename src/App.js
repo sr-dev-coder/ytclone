ea3f93ba512d830b2bc1478cfea5d7604d9b1910
Sandeep Rohilla <81100360+sr-dev-coder@users.noreply.github.com>
2023-02-04 23:41:46 +0530

    fix minor bugs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <Box sx={{ backgroundColor: '#000' }} >
         <Navbar />
         <Routes>
-            <Route path="/" exact element={<Feed />} />
+            <Route path="/" exact element={<Feed />} /> 
             <Route path="/video/:id" element={<VideoDetail />} />
             <Route path="/channel/:id" element={<ChannelDetail />} />
             <Route path="/search/:searchterm" element={<SearchFeed />} />
